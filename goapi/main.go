bc05a928b2caeca6511f35c1e1d7d6c612910317
Jan Härkönen <jan.harkonen@gmail.com>
2025-06-27 18:48:42 +0300

    add basic goapi code

---
+++ goapi/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Starting Go API")
+	router := gin.Default()
+
+	router.Use(corsConfig)
+	router.GET("/ping", func(c *gin.Context) { c.String(http.StatusOK, "pong\n") })
+	router.GET("/data", getData)
+	router.Run(":8080")
+}
+
+func corsConfig(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+	c.Next()
+}
+
+func getData(c *gin.Context) {
+	c.JSON(http.StatusOK, "data")
+}
