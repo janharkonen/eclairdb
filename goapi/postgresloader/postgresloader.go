dbfd9d541c832f95aa694c13d4e8aa7cd10f33a2
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-01 10:22:20 +0300

    change tests

--- goapi/postgresloader/postgresloader.go
+++ goapi/postgresloader/postgresloader.go
@@ -1,12 +1,6 @@
 package postgresloader
 
-import (
-	"fmt"
-)
-
 func LoadData() error {
-	fmt.Println("Loading data into PostgreSQL")
-	fmt.Println(addition(1, 2))
 	return nil
 }
 
--- goapi/postgresloader/postgresloader_test.go
+++ goapi/postgresloader/postgresloader_test.go
@@ -5,8 +5,14 @@ import (
 )
 
 func TestAddition(t *testing.T) {
-	result := addition(1, 2)
-	if result != 3 {
+	result := addition(5, 2)
+	if result != 7 {
 		t.Errorf("Expected 3, got %d", result)
+	}
+}
+func TestLoadData(t *testing.T) {
+	err := LoadData()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
 	}
 }
