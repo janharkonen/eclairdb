a5e49552cc95e71aa0e8e6a9fcfb5b00f8ef8e75
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-12 20:35:38 +0300

    remove unused assets

--- vuefrontend/src/App.vue
+++ vuefrontend/src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="text-2xl font-bold text-center text-green-600 p-4">
+    Welcome to EclairDB
+  </div>
 </template>
 
 <style scoped>
