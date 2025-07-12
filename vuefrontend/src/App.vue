3b48073538e3b03f83d2ca4ab6246d1f171dd63c
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-13 00:59:31 +0300

    center elements properly

--- vuefrontend/src/App.vue
+++ vuefrontend/src/App.vue
@@ -1,17 +1,27 @@
+<script setup>
+import Homepage from './Homepage.vue';
+</script>
+
 <template>
-  <header class="bg-gray-100">
-    <div class="text-2xl font-bold text-left text-green-600 p-4">
-      EclairDB
-    </div>
-  </header>
-  <main class="p-4">
-    <div class="flex items-center space-x-4 max-w-4xl mx-auto">
-      <img src="./assets/logo.svg" alt="EclairDB Logo" class="w-8 h-8 flex-shrink-0" />
-      <input 
-        type="text" 
-        placeholder="Enter URI here..." 
-        class="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-      />
-    </div>
+  <main class="w-full h-full p-4">
+    <Homepage />
   </main>
+  <footer className="flex-none w-full bg-gray-100 text-gray-800">
+    <div className="mx-auto py-4 px-4 sm:px-6 lg:px-8">
+      <p className="text-center text-sm text-gray-500">
+        © Jan Härkönen 2025
+      </p>
+    </div>
+  </footer>
 </template>
+
+<style scoped>
+/*
+header {
+  img {
+    max-width: 400px;
+    max-height: 400px;
+  }
+}
+*/
+</style>
---
+++ vuefrontend/src/Homepage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="w-full h-full flex flex-col">
+    <div class="logo-div">
+      <img 
+      src="https://picapi.janharkonen.fi/api/pics/ba7cbf1c9780435899c83df67d5a1747.png"
+      alt="EclairDB Logo"
+      class="logo-img" 
+      />
+    </div>
+    <div class="inputs-div">
+      <div class="input-div">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg"
+        alt="Postgres Logo" 
+        class="w-8 h-8 flex-shrink-0" 
+        />
+        <input 
+        type="text" 
+        placeholder="postgres://user_name:pass_word@host-IP:port/database-name?sslmode=disable" 
+        class="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo-div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+.logo-img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  max-width: 240px;
+  max-height: 240px;
+}
+.inputs-div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding-top: 8rem;
+  gap: 2rem;
+  width: 100%;
+  height: 100%;
+  flex-grow: 1;
+}
+.input-div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  width: 100%;
+  max-width: 920px;
+}
+</style>
