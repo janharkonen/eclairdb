605e85e76d6a58c1900b86bb315a76298d71891c
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-15 00:41:02 +0300

    dockerize frontend

--- stop_prod.sh
+++ stop_prod.sh
@@ -1,8 +1,10 @@
 docker compose down
 docker stop goapicontainer
+docker stop vuefrontend
 docker container rm goapicontainer
-docker container rm goapicontainer
+docker container rm vuefrontend
 docker image remove eclairdb-goapi:latest 
+docker image remove eclairdb-vuefrontend:latest 
 docker image remove --force janharkonen/eclairdb-goapi:latest 
 docker ps
 docker image ls
