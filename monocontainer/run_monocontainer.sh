3e2ce583c3b60119d7b1bb8bc553f6d52bf44f29
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-18 22:34:18 +0300

    configure monocontainer dockerfile

--- monocontainer/run_monocontainer.sh
+++ monocontainer/run_monocontainer.sh
@@ -1,4 +1,7 @@
 docker stop eclairdb-mono-container
 docker rm eclairdb-mono-container
-docker build -t eclairdb_mono .
+cd $(pwd)/../
+docker build -t eclairdb_mono . -f ./monocontainer/Dockerfile
 docker run -p 8080:80 --name eclairdb-mono-container eclairdb_mono
+cd $(pwd)/monocontainer
+
--- monocontainer/start.sh
+++ monocontainer/start.sh
@@ -2,6 +2,6 @@
 echo "Starting Go API"
 /usr/local/bin/goapi/main &
 echo "Starting Node.js"
-http-server ./dist -p 3000&
+http-server ./dist -p 3000 &
 echo "Starting Nginx"
 nginx -g "daemon off;"
