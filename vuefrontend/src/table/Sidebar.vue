a8cb8af2a4f02326ee61b5111483d3d4c269f2d6
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-21 01:47:45 +0300

    make niver ui for sidebar

--- vuefrontend/src/table/Sidebar.vue
+++ vuefrontend/src/table/Sidebar.vue
@@ -2,7 +2,7 @@
     <div class="text-white h-full w-full sidebar bg-cyan-800 p-4  overflow-y-auto border-r-2 border-cyan-300">
         <div v-for="schemaName in Object.keys(props.data)" :key="schemaName" class="mb-2">
             <div 
-                class="flex items-center cursor-pointer hover:bg-cyan-700 p-1 rounded"
+                class="flex items-center cursor-pointer hover:bg-cyan-700 p-1 rounded border border-cyan-600"
                 @click="toggleSchema(schemaName)"
             >
                 <span class="mr-2 select-none overflow-hidden text-ellipsis whitespace-nowrap">{{ isSchemaOpen[schemaName] ? `▼ ${schemaName}` : `► ${schemaName}` }}</span>
