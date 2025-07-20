21c6ec1d3e2cbe29e2abc3c603ea5cfdf541ee8e
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-20 18:53:01 +0300

    add basic resize for sidebar

--- vuefrontend/src/table/Table.vue
+++ vuefrontend/src/table/Table.vue
@@ -1,15 +1,31 @@
 <template>
   <div v-if="isLoading">Loading...</div>
   <div v-else-if="error">Error: {{ error.message }}</div>
-  <div v-else>
-    <p v-for="schema in Object.keys(data)">{{ schema }}</p>
+  <div v-else class="h-full flex flex-row w-full">
+    <div class="h-full flex"
+      ref="sidebarContainer"
+      :style="{ width: sidebarWidth + 'px' }"
+      >
+      <Sidebar :data="data" class="flex-grow" />
+    </div>
+    <div class="w-1 h-full cursor-ew-resize bg-cyan-300 hover:bg-cyan-400 active:bg-cyan-500" 
+      @mousedown="startResize"
+    ></div>
+    <div class="flex-1 h-full">
+      <p>asd</p>
+      <!-- Content area will go here -->
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { useQuery } from '@tanstack/vue-query'
-import { computed } from 'vue'
+import Sidebar from './Sidebar.vue'
+import { ref } from 'vue'
+
+
+const sidebarWidth = ref(160)
 const route = useRoute()
 const hash = route.params.hash
 
@@ -26,9 +42,27 @@ const { data, isLoading, error } = useQuery({
   },
 })
 
-const schemas = computed(() => {
-  return Object.keys(data.value)
-})
+const sidebarContainer = ref<HTMLElement | null>(null)
+
+const startResize = (event: MouseEvent) => {
+  const startX = event.clientX
+  const startWidth = sidebarContainer.value?.clientWidth ?? 0;
+
+  const handleMouseMove = (moveEvent: MouseEvent) => {
+    const currentX = moveEvent.clientX
+    const currentWidth = (startWidth ?? 0) + (currentX - startX)
+    sidebarWidth.value = Math.max(currentWidth, 80)
+  }
+
+  const handleMouseUp = () => {
+    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mouseup', handleMouseUp)
+  }
+
+  document.addEventListener('mousemove', handleMouseMove)
+  document.addEventListener('mouseup', handleMouseUp)
+}
+
 </script>
 
 <style scoped>
