43a54da0cd72e1cd4e64d366f2bcd1aa72b2485b
Jan Härkönen <jan.harkonen@gmail.com>
2025-08-05 19:51:02 +0300

    configure deployment configs

--- stop_prod.sh
+++ stop_prod.sh
@@ -1,10 +1,15 @@
 docker compose down
 docker stop goapicontainer
 docker stop vuefrontendcontainer
+docker stop nginxcontainer
 docker container rm goapicontainer
 docker container rm vuefrontendcontainer
+docker container rm nginxcontainer
 docker image remove eclairdb-goapi:latest 
 docker image remove eclairdb-vuefrontend:latest 
+docker image remove eclairdb-nginx:latest 
 docker image remove --force janharkonen/eclairdb-goapi:latest 
+docker image remove --force janharkonen/eclairdb-vuefrontend:latest 
+docker image remove --force janharkonen/eclairdb-nginx:latest
 docker ps
 docker image ls
